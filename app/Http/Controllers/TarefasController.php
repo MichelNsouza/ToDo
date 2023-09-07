327bb7efb9d203b3b27424ccc5e5f118b2a38888
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2023-09-07 12:38:27 -0300

    re-up repo

---
+++ app/Http/Controllers/TarefasController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+
+class TarefasController extends Controller
+{
+    public function index()
+    {
+       $listaDetarefas = Tarefa::all();
+       return view('todo.index',compact('listaDetarefas') );
+    }
+
+    public function store()
+    {
+        $tarefa = new Tarefa();
+
+        $novaTarefa = request()->get('tarefa');
+
+        if ($novaTarefa != ""){
+            $tarefa->descricao = $novaTarefa;
+            $tarefa->save();
+        }
+        return redirect()->back();
+    }
+
+    public function edit($id)
+    {
+        $tarefa = Tarefa::find($id);
+        $listaDetarefas = $tarefa::all();
+        return view('todo.index',compact('listaDetarefas', 'tarefa') );
+
+    }
+
+    public function update($id)
+    {
+        $tarefa = Tarefa::find($id);
+        $tarefa->descricao = request()->get('tarefa');
+        $tarefa->save();
+
+        return redirect()->back();
+
+    }
+    public function delete($id)
+    {
+        $tarefa = Tarefa::find($id);
+        $tarefa->delete();
+        return redirect()->back();
+    }
+}
---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="col-md-12">
+            <h1 class="mt-5 mb-5">ToDo list </h1>
+                <ul class="list-group">
+                    @foreach($listaDetarefas as $item)
+                        <li class="list-group-item">
+                            {{$item->descricao}} -
+                            <a class="btn btn-sm btn-primary" href="/editar/{{$item->id}}">Editar</a> -
+                            <a class="btn btn-sm btn-danger" href="/excluir/{{$item->id}}">Excluir</a>
+                        </li>
+                    @endforeach
+                </ul>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="mt-2 mb-5">Cadastrar tarefa</h2>
+                <form action="" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <textarea class="form-control" id=tarefa" name="tarefa" rows="3">{{$tarefa->descricao ?? ""}}</textarea>
+                        <button class="btn btn-primary mt-3">Cadastrar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+</html>
